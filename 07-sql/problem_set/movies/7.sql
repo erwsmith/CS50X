fca02af11ce43eb8269940c09b1db1684492194c
Eric Smith <erwsmith@gmail.com>
2022-08-31 10:09:55 -0600

    add alternate version

--- 07-sql/problem_set/movies/7.sql
+++ 07-sql/problem_set/movies/7.sql
@@ -2,3 +2,9 @@ SELECT title, rating FROM movies JOIN ratings
 ON movies.id = ratings.movie_id
 WHERE movies.year = 2010
 ORDER BY rating DESC, title;
+
+-- This does the same, abbreviating table names as shorter variables:
+SELECT title, rating FROM movies m JOIN ratings r
+ON m.id = r.movie_id
+WHERE m.year = 2010
+ORDER BY rating DESC, title;
